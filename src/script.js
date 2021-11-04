fa7e626ebcd2ae00cb014494834ea0a3283cd8e9
Manuel Ernesto Bonilla Muñoz <mebonilla9@gmail.com>
2021-11-03 23:59:15 -0500

    Added loader in glb extension

--- src/script.js
+++ src/script.js
@@ -1,12 +1,37 @@
 import './style.css'
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import Stats from 'three/examples/jsm/libs/stats.module.js'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import Stats from 'three/examples/jsm/libs/stats.module'
+import { GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
 
 // Stats
 const stats = Stats()
 document.body.appendChild(stats.dom)
 
+// Utils
+/**
+ * Convert a degrees to radians
+ * 
+ * @param {*} degrees Integer value defined in degrees
+ * @returns Integer values defined in radians
+ */
+const degreesToRadians = (degrees) => {
+	return degrees * (Math.PI / 180)
+}
+
+// Loader
+const loader = new GLTFLoader();
+
+loader.load('/assets/bottle.glb', (glb) => {
+    console.log(glb)
+    const root = glb.scene
+    root.position.y = 5
+    scene.add(root)
+}, (xhr) => {
+    console.log(xhr.loaded/xhr.total * 100 + '% loaded')
+}, (error) => {
+    console.error('An error ocurred: ' + error)
+})
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
@@ -33,10 +58,10 @@ scene.add(plane)
 
 // Lights
 const lightDirectional = new THREE.DirectionalLight(0xffffff, 1)
+lightDirectional.position.set(2, 2, 5)
 scene.add(lightDirectional)
-lightDirectional.position.set(5, 5, 5)
 
-const lightAmbient = new THREE.AmbientLight(0x9eaeff, 0.2)
+const lightAmbient = new THREE.AmbientLight(0xffffff, 0.4)
 scene.add(lightAmbient)
 
 /**
@@ -61,16 +86,12 @@ window.addEventListener('resize', () => {
 	renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
-const degreesToRadians = (degrees) => {
-	return degrees * (Math.PI / 180)
-}
-
 /**
  * Camera
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.5, 10000)
-camera.position.set(0,10,50)
+camera.position.set(0,10,30)
 scene.add(camera)
 
 // Controls
